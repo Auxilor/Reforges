6c7059c49d79cd84b4aa425bf1c9f092670da5f8
Auxilor <william.favierparsons1@gmail.com>
2021-10-31 10:55:00 +0000

    Added EffectActivateEvent javadoc

--- eco-core/core-plugin/src/main/java/com/willfp/reforges/events/EffectActivateEvent.java
+++ eco-core/core-plugin/src/main/java/com/willfp/reforges/events/EffectActivateEvent.java
@@ -9,49 +9,98 @@ import org.bukkit.event.player.PlayerEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class EffectActivateEvent extends PlayerEvent implements Cancellable {
-
+    /**
+     * The reforge.
+     */
     private final Reforge reforge;
 
+    /**
+     * The effect that activated.
+     */
     private final Effect effect;
 
+    /**
+     * If the event is cancelled.
+     */
     private boolean cancelled;
 
+    /**
+     * Bukkit parity.
+     */
     private static final HandlerList HANDLERS = new HandlerList();
 
-    public EffectActivateEvent(@NotNull Player who, @NotNull Reforge reforge, @NotNull Effect effect) {
+    /**
+     * Create new EffectActivateEvent.
+     *
+     * @param who     The player.
+     * @param reforge The reforge.
+     * @param effect  The effect.
+     */
+    public EffectActivateEvent(@NotNull final Player who,
+                               @NotNull final Reforge reforge,
+                               @NotNull final Effect effect) {
         super(who);
         this.reforge = reforge;
         this.effect = effect;
         this.cancelled = false;
     }
 
+    /**
+     * Get if the effect activation is cancelled.
+     *
+     * @return If cancelled.
+     */
     @Override
     public boolean isCancelled() {
         return this.cancelled;
     }
 
+    /**
+     * Set if the event is cancelled.
+     *
+     * @param cancel If cancelled.
+     */
     @Override
-    public void setCancelled(boolean cancel) {
+    public void setCancelled(final boolean cancel) {
         this.cancelled = cancel;
     }
 
+    /**
+     * Bukkit parity.
+     *
+     * @return The handlers.
+     */
     @Override
     public @NotNull HandlerList getHandlers() {
         return HANDLERS;
     }
 
+    /**
+     * Get the reforge associated with the event.
+     *
+     * @return The reforge.
+     */
     @NotNull
     public Reforge getReforge() {
         return this.reforge;
     }
 
+    /**
+     * Get the effect associated with the event.
+     *
+     * @return The effect.
+     */
     @NotNull
     public Effect getEffect() {
         return this.effect;
     }
 
+    /**
+     * Bukkit parity.
+     *
+     * @return The handler list.
+     */
     public static HandlerList getHandlerList() {
         return HANDLERS;
     }
-
 }
