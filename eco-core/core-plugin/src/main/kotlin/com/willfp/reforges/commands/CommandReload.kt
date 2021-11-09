df72cd0e577bcd924a918a88e3b34685fa22511f
Auxilor <william.favierparsons1@gmail.com>
2021-11-09 13:38:53 +0000

    Added %time% placeholder to reload message

--- eco-core/core-plugin/src/main/kotlin/com/willfp/reforges/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/reforges/commands/CommandReload.kt
@@ -3,12 +3,13 @@ package com.willfp.reforges.commands
 import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.command.CommandHandler
 import com.willfp.eco.core.command.impl.Subcommand
+import com.willfp.eco.util.StringUtils
 
 class CommandReload(plugin: EcoPlugin) : Subcommand(plugin, "reload", "reforges.command.reload", false) {
     override fun getHandler(): CommandHandler {
         return CommandHandler { sender, _ ->
-            plugin.reload()
-            sender.sendMessage(plugin.langYml.getMessage("reloaded"))
+            sender.sendMessage(plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                .replace("%time%", plugin.reloadWithTime().toString()))
         }
     }
 }
