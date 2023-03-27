b91e8590064f3216beacbc495c3b66f82387e7f3
Auxilor <william.favierparsons1@gmail.com>
2023-03-27 18:43:17 +0100

    Fixed caffeine

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -4,7 +4,7 @@ version = rootProject.version
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
     compileOnly("com.willfp:Talismans:6.0.0")
-    compileOnly("com.benmanes.caffeine:caffeine:3.0.2")
+    compileOnly("com.github.ben-manes.caffeine:caffeine:3.0.2")
 
     implementation("com.willfp:ecomponent:1.3.0")
 }
