f3d1670e6cfca76ed0982299634be0700765c072
Auxilor <william.favierparsons1@gmail.com>
2023-03-28 14:57:45 +0100

    Fixed publications

--- eco-core/core-plugin/src/main/kotlin/com/willfp/reforges/reforges/Reforges.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/reforges/reforges/Reforges.kt
@@ -1,17 +1,12 @@
 package com.willfp.reforges.reforges
 
 import com.google.common.collect.ImmutableSet
-import com.willfp.eco.core.config.ConfigType
 import com.willfp.eco.core.config.interfaces.Config
-import com.willfp.eco.core.config.readConfig
-import com.willfp.eco.core.config.updating.ConfigUpdater
 import com.willfp.eco.core.registry.Registry
-import com.willfp.libreforge.chains.EffectChains
 import com.willfp.libreforge.loader.LibreforgePlugin
 import com.willfp.libreforge.loader.configs.ConfigCategory
 import com.willfp.libreforge.loader.configs.LegacyLocation
 import com.willfp.reforges.ReforgesPlugin
-import java.io.File
 
 @Suppress("UNUSED")
 object Reforges : ConfigCategory("reforge", "reforges") {
